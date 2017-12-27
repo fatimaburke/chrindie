af1e94ab81fdeda44e5388206487b481c5c76f54
Fatima Burke <fatima@lurmarketing.com>
2017-12-27 12:27:21 -0500

    Model work

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,9 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  has_many :tracks
+  has_many :tracks, :dependent => :destroy
+
+  has_attached_file :track, :restricted_characters => /[&$+,\/:;=?@<>\[\]\{\}\|\\\^~%#]/, dependent: :destroy
+  validates_attachment_presence :track
+  validates_attachment_content_type :track, :content_type => [ 'audio/mpeg', 'audio/mp3' ]
+  validates_attachment_size :track, :less_than => 20.megabytes
 end
