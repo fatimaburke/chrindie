a841c0f7eea2a043a10107f30be6e164b1710758
Fatima Burke <fatima@lurmarketing.com>
2017-12-26 19:41:32 -0500

    Added Artists

---
+++ app/models/artist.rb
@@ -0,0 +1,9 @@
+class Artist < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :albums
+  has_many :playlists
+end
