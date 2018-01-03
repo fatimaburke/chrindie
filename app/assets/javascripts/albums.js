2079691e9163746e92c675e174d5dd34561c0cfb
Fatima Burke <fatima@lurmarketing.com>
2018-01-02 20:04:30 -0500

    Main function for player redone to work with playlists and to pull in JSON attachment data

---
+++ app/assets/javascripts/albums.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  new jPlayerPlaylist({
+    jPlayer: '#jquery_jplayer_1',
+    cssSelectorAncestor: '#jp_container_1'
+  }, allTracks, {
+    swfPath: '/js',
+    supplied: 'mp3',
+    wmode: 'window',
+    useStateClassSkin: true,
+    autoBlur: false,
+    smoothPlayBar: true,
+    keyEnabled: true
+  });
+});
