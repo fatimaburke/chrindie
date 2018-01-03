8035567ce6b194b7fbedf79728ace1b22bce5ee4
Fatima Burke <fatima@lurmarketing.com>
2018-01-03 00:52:20 -0500

    Added loading through turbolinks

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-  new jPlayerPlaylist({
-    jPlayer: '#jquery_jplayer_1',
-    cssSelectorAncestor: '#jp_container_1'
-  }, allTracks, {
-    swfPath: '/js',
-    supplied: 'mp3',
-    wmode: 'window',
-    useStateClassSkin: true,
-    autoBlur: false,
-    smoothPlayBar: true,
-    keyEnabled: true
-  });
-});
+document.addEventListener("turbolinks:load", function() {
+// $(document).ready(function() {
+    new jPlayerPlaylist({
+      jPlayer: '#jquery_jplayer_1',
+      cssSelectorAncestor: '#jp_container_1'
+    }, allTracks, {
+      swfPath: '/js',
+      supplied: 'mp3',
+      wmode: 'window',
+      useStateClassSkin: true,
+      autoBlur: false,
+      smoothPlayBar: true,
+      keyEnabled: true
+    });
+  // });
+})
