d9bd208ef9f1593956986528e097dc64bcb5f83d
Fatima Burke <fatima@lurmarketing.com>
2018-01-26 10:16:33 -0500

    Configured custom registration page for artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :artists
+  devise_for :artists,
+             :controllers => {
+                :registrations => 'registrations'
+              }
   devise_for :users
   get 'home/index'
 
